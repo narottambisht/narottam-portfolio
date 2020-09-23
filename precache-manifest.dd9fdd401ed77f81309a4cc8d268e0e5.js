c6338f062c9b05b09de8c83de15792537852c9e3
narottambisht <bishtnarottam@gmail.com>
2020-09-23 19:32:30 +0530

    Updates

---
+++ precache-manifest.dd9fdd401ed77f81309a4cc8d268e0e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9210c58eab87250fb6f2d36b527a75c",
+    "url": "/narottam-portfolio/index.html"
+  },
+  {
+    "revision": "ffe311560a1b70e79060",
+    "url": "/narottam-portfolio/static/css/main.a1270b51.chunk.css"
+  },
+  {
+    "revision": "9ecbc8b032d4b7c2d03a",
+    "url": "/narottam-portfolio/static/js/2.62b79ad5.chunk.js"
+  },
+  {
+    "revision": "014c8388aa3aa7d38ce44459649a48eb",
+    "url": "/narottam-portfolio/static/js/2.62b79ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe311560a1b70e79060",
+    "url": "/narottam-portfolio/static/js/main.4f5e8927.chunk.js"
+  },
+  {
+    "revision": "7ebbe5ca1285bad76530",
+    "url": "/narottam-portfolio/static/js/runtime-main.7736b836.js"
+  }
+]);
